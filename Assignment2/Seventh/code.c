e07093205054e3b20820b1b70308f6779a42af69
PujitaGarg <pujita082@gmail.com>
2025-03-18 19:02:39 +0530

    Update

--- Assignment2/Seventh/code.c
+++ Assignment2/Seventh/code.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-// Function to compute the prefix sum of an array
+
 void prefix_sum(int *array, int n) {
     for (int i = 1; i < n; i++) {
         array[i] += array[i - 1];
@@ -19,16 +19,33 @@ int main(int argc, char **argv) {
     int n = 16; // Size of the global array
     int local_n = n / size; // Size of the local array for each process
 
+    int *global_array = NULL;
+    if (rank == 0) {
+        global_array = (int *)malloc(n * sizeof(int));
+        for (int i = 0; i < n; i++) {
+            global_array[i] = i + 1;
+        }
+        printf("Initial Array: ");
+        for (int i = 0; i < n; i++) {
+            printf("%d ", global_array[i]);
+        }
+        printf("\n");
+    }
+
     // Allocate memory for the local array
     int *local_array = (int *)malloc(local_n * sizeof(int));
 
-    // Initialize the local array with some values
-    for (int i = 0; i < local_n; i++) {
-        local_array[i] = rank * local_n + i + 1;
-    }
+    // Scatter the global array to all processes
+    MPI_Scatter(global_array, local_n, MPI_INT, local_array, local_n, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Compute the local prefix sum
     prefix_sum(local_array, local_n);
+
+    printf("Process %d: Local prefix sum = ", rank);
+    for (int i = 0; i < local_n; i++) {
+        printf("%d ", local_array[i]);
+    }
+    printf("\n");
 
     // Gather the last element of each local prefix sum
     int *last_elements = NULL;
@@ -47,8 +64,10 @@ int main(int argc, char **argv) {
     MPI_Scatter(last_elements, 1, MPI_INT, &offset, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Add the offset to the local prefix sum
-    for (int i = 0; i < local_n; i++) {
-        local_array[i] += offset;
+    if (rank != 0) {
+        for (int i = 0; i < local_n; i++) {
+            local_array[i] += offset;
+        }
     }
 
     // Gather the final prefix sums from all processes
@@ -65,8 +84,10 @@ int main(int argc, char **argv) {
             printf("%d ", global_prefix_sum[i]);
         }
         printf("\n");
+
         free(global_prefix_sum);
         free(last_elements);
+        free(global_array);
     }
 
     free(local_array);
